7a181d018404f8d8bfaf03c2dd823d62c24a8c78
Ken Sodemann <ken.sodemann@gmail.com>
2026-06-15 03:56:31 -0500

    create the auth composable

---
+++ src/core/__mocks__/authentication.ts
@@ -0,0 +1,25 @@
+import { vi } from 'vitest';
+import { ref } from 'vue';
+
+const isAuthenticated = vi.fn().mockResolvedValue(false);
+const login = vi.fn().mockResolvedValue({});
+const logout = vi.fn().mockResolvedValue(undefined);
+const sendPasswordReset = vi.fn().mockResolvedValue(undefined);
+const changePassword = vi.fn().mockResolvedValue(undefined);
+
+export const useAuthentication: () => any = () => {
+  return {
+    isAuthenticated,
+    login,
+    logout,
+    sendPasswordReset,
+    changePassword,
+    user: ref({
+      id: 1,
+      created_at: 1781274921674,
+      name: 'Testy McDefaultTestUser',
+      email: 'test@testy-face.com',
+      role: 'admin',
+    }),
+  };
+};
