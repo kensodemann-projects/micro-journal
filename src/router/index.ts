d40e8c55fdc270b4e6c545b5aafc78a2852e4359
Ken Sodemann <ken.sodemann@gmail.com>
2026-06-18 05:44:01 -0500

    protect any route that does not explicitly allow anonymous access

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,12 +5,24 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router';
 import { routes } from 'vue-router/auto-routes';
+import { useAuthentication } from '@/core/authentication';
+
+const checkAuthStatus = async (to: RouteLocationNormalized) => {
+  if (!to.meta.allowAnonymous) {
+    const { isAuthenticated } = useAuthentication();
+    if (!(await isAuthenticated())) {
+      return '/login';
+    }
+  }
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+router.beforeEach(checkAuthStatus);
 
 export default router;
