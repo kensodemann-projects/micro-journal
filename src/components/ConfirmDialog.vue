46fc483c0c7e899c6fdb9c3d7c190b8fd4f65f05
Ken Sodemann <ken.sodemann@gmail.com>
2026-07-05 06:29:30 -0500

    WIP - add the modal construct

--- src/components/ConfirmDialog.vue
+++ src/components/ConfirmDialog.vue
@@ -1,22 +1,25 @@
 <template>
-  <v-card>
-    <v-card-title data-testid="title">Are you sure?</v-card-title>
-    <v-card-text data-testid="body">
-      <div class="d-flex ga-4 align-center">
-        <div><v-icon icon="mdi-help-rhombus" size="64" :color="iconColor || 'info'" /></div>
-        <div>
-          {{ question }}
+  <v-dialog v-model="showConfirmDialog" max-width="600px" data-testid="confirm-dialog">
+    <v-card>
+      <v-card-title data-testid="title">Are you sure?</v-card-title>
+      <v-card-text data-testid="body">
+        <div class="d-flex ga-4 align-center">
+          <div><v-icon icon="mdi-help-rhombus" size="64" :color="iconColor || 'info'" /></div>
+          <div>
+            {{ question }}
+          </div>
         </div>
-      </div>
-    </v-card-text>
-    <v-card-actions>
-      <v-btn color="primary" @click="$emit('confirm')" data-testid="yes-button">Yes</v-btn>
-      <v-btn color="secondary" @click="$emit('cancel')" data-testid="no-button">No</v-btn>
-    </v-card-actions>
-  </v-card>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="$emit('confirm')" data-testid="yes-button">Yes</v-btn>
+        <v-btn color="secondary" @click="$emit('cancel')" data-testid="no-button">No</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
+const showConfirmDialog = defineModel<boolean>({ required: true });
 defineProps<{ question: string; iconColor?: string }>();
 defineEmits(['confirm', 'cancel']);
 </script>
--- src/components/__tests__/ConfirmDialog.spec.ts
+++ src/components/__tests__/ConfirmDialog.spec.ts
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils';
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils';
 import { afterEach, describe, expect, it, vi } from 'vitest';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
@@ -7,10 +7,14 @@ import ConfirmDialog from '../ConfirmDialog.vue';
 
 describe('Confirm Dialog', () => {
   const vuetify = createVuetify({ components, directives });
-  const mountComponent = (question: string) =>
-    mount(ConfirmDialog, { global: { plugins: [vuetify] }, props: { question } });
+  const mountComponent = async (question: string) => {
+    const wrapper = mount(ConfirmDialog, { global: { plugins: [vuetify] }, props: { question, modelValue: true } });
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    return wrapper;
+  };
 
-  let wrapper: ReturnType<typeof mountComponent>;
+  let wrapper: VueWrapper<InstanceType<typeof ConfirmDialog>>;
 
   afterEach(() => {
     wrapper?.unmount();
@@ -19,21 +23,22 @@ describe('Confirm Dialog', () => {
       vi.useRealTimers();
     } catch {}
   });
-  it('displays the question', () => {
+
+  it('displays the question', async () => {
     const question = 'This is the question that I will ask?';
-    wrapper = mountComponent(question);
+    wrapper = await mountComponent(question);
     expect(wrapper.find('[data-testid="body"]').text()).toBe(question);
   });
 
   it('emits cancel on no pressed', async () => {
-    wrapper = mountComponent('This is the question that I will ask?');
+    wrapper = await mountComponent('This is the question that I will ask?');
     const button = wrapper.find('[data-testid="no-button"]');
     await button.trigger('click');
     expect(wrapper.emitted('cancel')).toBeTruthy();
   });
 
   it('emits confirm on yes pressed', async () => {
-    wrapper = mountComponent('This is the question that I will ask?');
+    wrapper = await mountComponent('This is the question that I will ask?');
     const button = wrapper.find('[data-testid="yes-button"]');
     await button.trigger('click');
     expect(wrapper.emitted('confirm')).toBeTruthy();
